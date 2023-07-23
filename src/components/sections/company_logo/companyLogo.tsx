6422f7e2e07b56d2bdd3d452f94107916b8d6b4e
Jay Sanduke <Jaysanduke@gmail.com>
2023-07-23 10:33:43 +0530

    updates

--- src/components/sections/company_logo/companyLogo.tsx
+++ src/components/sections/company_logo/companyLogo.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image"
 import "./style.css"
 
+
 export default function CompanyLogo() {
     return (
         <section id="companies-logo" className="relative flex flex-col items-center py-16 lg:py-12 lg:px-10 lg:flex-row lg:justify-center">
@@ -8,34 +9,63 @@ export default function CompanyLogo() {
                 <div className="setion-heading text-center">
                     <h2 className="text-3xl font-bold uppercase mb-10 text-gray-800">Companies</h2>
                 </div>
-                <div className="logo-container overflow-x-hidden py-10">
-                    <div className="logo-slider whitespace-nowrap">
-                        <div className="logo-wrapper">
-                            <Image src="/companies_Logo/Amneal.png" className="h-[120px] w-auto" alt="Amneal" width={200} height={120} />
+                <div className="logo-container w-full py-10 whitespace-nowrap overflow-x-hidden">
+                    <div className="logo-slider inline-block whitespace-nowrap">
+                        <div className="logo-wrapper inline-flex justify-center items-center mx-36 h-[120px] w-[280px]">
+                            <Image src={"/Companies_Logo/Amneal.png"} className="h-full w-auto" alt="Amneal" width={390} height={162} />
                         </div>
-                        <div className="logo-wrapper">
-                            <Image src="/Companies_Logo/Apollo__hospitals.png" className="h-[120px] w-auto" alt="Apollo hospitals" width={200} height={120} />
+                        <div className="logo-wrapper inline-flex justify-center items-center mx-36 h-[120px] w-[280px]">
+                            <Image src="/Companies_Logo/Apollo__hospitals.png" className="h-full w-auto" alt="Apollo hospitals" width={2400} height={2616} />
                         </div>
-                        <div className="logo-wrapper">
-                            <Image src="/Companies_Logo/Bank_Note_Press_Dewas.png" className="h-[120px] w-auto" alt="Bank Note Press Dewas" width={200} height={120} />
+                        <div className="logo-wrapper inline-flex justify-center items-center mx-36 h-[120px] w-[280px]">
+                            <Image src="/Companies_Logo/Bank_Note_Press_Dewas.png" className="h-full w-auto" alt="Bank Note Press Dewas" width={450} height={450} />
                         </div>
-                        <div className="logo-wrapper">
-                            <Image src="/Companies_Logo/Biochem_Pharma.png" className="h-[120px] w-auto" alt="Biochem Pharma" width={200} height={120} />
+                        <div className="logo-wrapper inline-flex justify-center items-center mx-36 h-[120px] w-[280px]">
+                            <Image src="/Companies_Logo/Biochem_Pharma.png" className="h-full w-auto" alt="Biochem Pharma" width={5000} height={1900} />
                         </div>
-                        <div className="logo-wrapper">
-                            <Image src="/Companies_Logo/BridgeStone.png" className="h-[120px] w-auto" alt="BridgeStone" width={200} height={120} />
+                        <div className="logo-wrapper inline-flex justify-center items-center mx-36 h-[120px] w-[280px]">
+                            <Image src="/Companies_Logo/BridgeStone.png" className="h-full w-auto" alt="BridgeStone" width={2400} height={1600} />
                         </div>
-                        <div className="logo-wrapper">
-                            <Image src="/Companies_Logo/Cooper_Pharma_Ltd.png" className="h-[120px] w-auto" alt="Cooper Pharma Ltd" width={200} height={120} />
+                        <div className="logo-wrapper inline-flex justify-center items-center mx-36 h-[120px] w-[280px]">
+                            <Image src="/Companies_Logo/Cooper_Pharma_Ltd.png" className="h-full w-auto" alt="Cooper Pharma Ltd" width={2000} height={841} />
                         </div>
-                        <div className="logo-wrapper">
-                            <Image src="/Companies_Logo/ICAI.png" className="h-[120px] w-auto" alt="ICAI" width={200} height={120} />
+                        <div className="logo-wrapper inline-flex justify-center items-center mx-36 h-[120px] w-[280px]">
+                            <Image src="/Companies_Logo/ICAI.png" className="h-full w-auto" alt="ICAI" width={912} height={916} />
                         </div>
-                        <div className="logo-wrapper">
-                            <Image src="/Companies_Logo/MSN.png" className="h-[120px] w-auto" alt="MSN Laboratories Ltd" width={200} height={120} />
+                        <div className="logo-wrapper inline-flex justify-center items-center mx-36 h-[120px] w-[280px]">
+                            <Image src="/Companies_Logo/MSN.png" className="h-full w-auto" alt="MSN Laboratories Ltd" width={1540} height={670} />
                         </div>
-                        <div className="logo-wrapper">
-                            <Image src="/Companies_Logo/Zydus_Cadila.png" className="h-[120px] w-auto" alt="Zydus Cadila" width={200} height={120} />
+                        <div className="logo-wrapper inline-flex justify-center items-center mx-36 h-[120px] w-[280px]">
+                            <Image src="/Companies_Logo/Zydus_Cadila.png" className="h-full w-auto" alt="Zydus Cadila" width={600} height={90} />
+                        </div>
+                    </div>
+                    <div className="logo-slider inline-block whitespace-nowrap">
+                        <div className="logo-wrapper inline-flex justify-center items-center mx-36 h-[120px] w-[280px]">
+                            <Image src={"/Companies_Logo/Amneal.png"} className="h-full w-auto" alt="Amneal" width={390} height={162} />
+                        </div>
+                        <div className="logo-wrapper inline-flex justify-center items-center mx-36 h-[120px] w-[280px]">
+                            <Image src="/Companies_Logo/Apollo__hospitals.png" className="h-full w-auto" alt="Apollo hospitals" width={2400} height={2616} />
+                        </div>
+                        <div className="logo-wrapper inline-flex justify-center items-center mx-36 h-[120px] w-[280px]">
+                            <Image src="/Companies_Logo/Bank_Note_Press_Dewas.png" className="h-full w-auto" alt="Bank Note Press Dewas" width={450} height={450} />
+                        </div>
+                        <div className="logo-wrapper inline-flex justify-center items-center mx-36 h-[120px] w-[280px]">
+                            <Image src="/Companies_Logo/Biochem_Pharma.png" className="h-full w-auto" alt="Biochem Pharma" width={5000} height={1900} />
+                        </div>
+                        <div className="logo-wrapper inline-flex justify-center items-center mx-36 h-[120px] w-[280px]">
+                            <Image src="/Companies_Logo/BridgeStone.png" className="h-full w-auto" alt="BridgeStone" width={2400} height={1600} />
+                        </div>
+                        <div className="logo-wrapper inline-flex justify-center items-center mx-36 h-[120px] w-[280px]">
+                            <Image src="/Companies_Logo/Cooper_Pharma_Ltd.png" className="h-full w-auto" alt="Cooper Pharma Ltd" width={2000} height={841} />
+                        </div>
+                        <div className="logo-wrapper inline-flex justify-center items-center mx-36 h-[120px] w-[280px]">
+                            <Image src="/Companies_Logo/ICAI.png" className="h-full w-auto" alt="ICAI" width={912} height={916} />
+                        </div>
+                        <div className="logo-wrapper inline-flex justify-center items-center mx-36 h-[120px] w-[280px]">
+                            <Image src="/Companies_Logo/MSN.png" className="h-full w-auto" alt="MSN Laboratories Ltd" width={1540} height={670} />
+                        </div>
+                        <div className="logo-wrapper inline-flex justify-center items-center mx-36 h-[120px] w-[280px]">
+                            <Image src="/Companies_Logo/Zydus_Cadila.png" className="h-full w-auto" alt="Zydus Cadila" width={600} height={90} />
                         </div>
                     </div>
                 </div>
