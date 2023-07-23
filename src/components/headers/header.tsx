b6f88a9a03b69d4968f4e26290ca2ecedbd341e5
Jay Sanduke <Jaysanduke@gmail.com>
2023-07-23 10:53:04 +0530

    Update header.tsx

--- src/components/headers/header.tsx
+++ src/components/headers/header.tsx
@@ -15,7 +15,7 @@ export default function Header() {
     <nav id="navbar" className="navbar bg-white">
       <div className="container mx-auto px-2 sm:px-6 lg:px-8">
         <div className="relative flex h-16 items-center justify-between">
-          <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+          <div className="absolute inset-y-0 left-0 items-center hidden">
             {/* Mobile menu button*/}
             <button type="button" className="inline-flex items-center justify-center rounded-md p-2 text-gray-500 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false"
               onClick={() => setIsOpen(!isOpen)}
@@ -39,7 +39,7 @@ export default function Header() {
               </svg>
             </button>
           </div>
-          <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+          <div className="relative flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
             <div className="flex flex-shrink-0 items-center">
               <Image
                 src="/Logos/Logo.png"
