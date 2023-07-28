623891d75282b287035b166f3161066a7fcce918
Jay Sanduke <Jaysanduke@gmail.com>
2023-07-28 20:01:44 +0530

    updates

--- src/components/sections/services/services.tsx
+++ src/components/sections/services/services.tsx
@@ -3,12 +3,12 @@ export default function Services() {
         <section id="services" className="relative bg-slate-200 flex w-full justify-center py-16">
             <div className="container lg:max-w-7xl">
                 <div className="section-heading flex justify-center mb-2">
-                    <h2 className="text-lg uppercase text-gray-800">Contact Us</h2>
+                    <h2 className="text-lg uppercase text-gray-800">Services</h2>
                 </div>
                 <div className="section-sub-heading text-center">
                     <h2 className="text-4xl font-bold text-gray-800">What we can offer you</h2>
                 </div>
-                <div className="relative grid gap-4">
+                <div className="relative grid grid-col gap-4">
                 </div>
             </div>
         </section>
